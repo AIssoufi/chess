3295c7bc2ff8683e4607addae3e6c88150309b57
issoufi adam <issoufi.adam@etu.unice.fr>
2018-05-10 12:25:39 +0200

    Mise à jour et documentation de la classe Plateau

--- app/js/Plateau.js
+++ app/js/Plateau.js
@@ -1,97 +1,50 @@
-import cavalier from './pieces/cavalier.js';
-import dame from './pieces/dame.js';
-import tour from './pieces/tour.js';
-import pion from './pieces/pion.js';
-import roi from './pieces/roi.js';
-import fou from './pieces/fou.js';
-
+/**
+ * Le plateau permet de réaliser tous les mouvements
+ * possibles (déplacement, capture, etc)
+ */
 export default class Plateau {
+  /**
+   * Colorer tous les déplacements possibles d'une pièce
+   * @param {String} positionOrigine Position de la pièce | Exemple: e3
+   * @param {Array} tabCheminPossible Tableau des trajectoirs (vecteur) possible
+   */
+  affihcerChemin(positionOrigine, tabCheminPossible) {
+    const caseSelectioneeNode = this.getCaseSelectionneeNode();
+    const caseCibleNode       = this.getCaseNode(positionOrigine);
 
-  placerNode(position, pièceNode) {
-    const caseNode = this.getCaseNode(position);
-    caseNode.appendChild(pièceNode);
-  }
+    if (caseCibleNode != caseSelectioneeNode) { return; }
+    
+    this.decolorerCase();
 
-  getCaseNode(position) {
-    return document.querySelector(`.case[data-position="${position}"]`);
-  }
+    let inverser = false;
+    const pièce  = this.getPièceNode(positionOrigine);
+    if (pièce) { inverser = this.getCouleurJoueurCourant() == "blanche"; }
 
-  getPièce(position) {
-    const caseNode = this.getCaseNode(position);
-
-    if (caseNode) {
-        return caseNode.childNodes[0];
-    }
-  }
-
-  getCouleur(position) {
-    const pièce = this.getPièce(position);
-    if (pièce) {
-        return pièce.dataset.couleur;
-    }
-  }
-
-  getCouleurJoueurCourant() {
-      const div = document.querySelector(".joueur-courant");
-      if (div) {
-          return div.dataset.couleur;
-      }
-  }
-
-  selectionnerCase(position) {
-    const caseNode = this.getCaseNode(position);
-    if(caseNode && caseNode.hasChildNodes()) {
-        const pièce = this.getPièce(position);
-        const couleurJoueurCourant = document.querySelector('.joueur-courant').dataset.couleur;
-
-        if(couleurJoueurCourant == pièce.dataset.couleur) {
-            this.deselectionnerCase();
-            caseNode.classList.add('selectionner');
-        } else {
-            console.log("Sélection non autorisée");
+    for(let uneDirection of tabCheminPossible) {
+        const postions = this.getDeplacementsPossible(positionOrigine, uneDirection, inverser);
+        for(let postion of postions) {
+            this.colorerCase(postion);
         }
     }
   }
+  /**
+   * Capturer une pièce
+   * @param {String} positionVictime La position de la victime | Exemple: a1
+   * @param {String} positionOppresseur La position de l'oppresseur | Exemple: a1
+   */
+  capturer(positionVictime, positionOppresseur) {
+    const victimeNode    = this.getCaseNode(positionVictime);
+    const oppresseurNode = this.getCaseNode(positionOppresseur);
 
-  deselectionnerCase() {
-    const caseNode = this.getCaseSelectionnee();
-    if(caseNode) {
-        caseNode.classList.remove('selectionner');
-        this.decolorerCase();
-    }
-  }
-
-  getCaseSelectionnee() {
-      return document.querySelector('.selectionner');
-  }
-
-  deplacer(from, to) {
-    const toNode = this.getCaseNode(to);
-    const fromNode = this.getCaseNode(from);
-
-    if (!toNode.classList.contains("chemin") || !fromNode.hasChildNodes()) {
-        console.log("Déplacement pas possible !");
-    } else {
-        toNode.appendChild(fromNode.childNodes[0]);
-        console.log(`Déplacement ${from} vers ${to}`);
-    }
-
-    this.deselectionnerCase();
-  }
-
-  capturer(victimePos, remplacentPos) {
-    const v = this.getCaseNode(victimePos);
-    const r = this.getCaseNode(remplacentPos);
-
-    if (!v.classList.contains("chemin")) {
+    if (!victimeNode.classList.contains("chemin")) {
         this.deselectionnerCase();
         console.log("Opération non possible !");
         return;
     } 
 
-    document.querySelector(`.joueur-courant [id*="prisoniers"]`).appendChild(v.childNodes[0]);
-    v.appendChild(r.childNodes[0]);
-    console.log(`${remplacentPos} a capturé ${victimePos}`);
+    document.querySelector(`.joueur-courant [id*="prisoniers"]`).appendChild(victimeNode.childNodes[0]);
+    victimeNode.appendChild(oppresseurNode.childNodes[0]);
+    console.log(`${positionOppresseur} a capturé ${positionVictime}`);
     this.deselectionnerCase();
 
     const nbPrisonier = document.querySelector(".joueur-courant [id*='nb-prisonier']");
@@ -99,179 +52,229 @@ export default class Plateau {
   }
 
   /**
-   * Colorer tous les déplacements possibles d'une pièce
-   * @param {*} positionDeDepart Position de la pièce cible | Exemple: e3
-   * @param {*} cheminPossible Tableau des trajectoirs (vecteur) possible
+   * Colorer une seul case
+   * @param {object} postion  La position de la case | Exemple: a1
    */
-  affihcerChemin(positionDeDepart, cheminPossible) {
-    const caseSelectionee = this.getCaseSelectionnee();
-    const caseCible = this.getCaseNode(positionDeDepart);
+  colorerCase(postion) {
+    if (postion[1] < 1 || postion[1] > 8 || postion[0] < 'a' || postion[0] > 'h') { return; }
 
-    if (caseCible != caseSelectionee) { return; }
-    
-    this.decolorerCase();
+    const caseNode = this.getCaseNode(postion);
+    if (caseNode) { caseNode.classList.add('chemin'); }
+  }
 
-    let inverser = false;
-    const pièce = this.getPièce(positionDeDepart);
-    if (pièce) { inverser = this.getCouleurJoueurCourant() == "blanche"; }
+  /**
+   *  Décolore toutes les cases qui sont colorées.
+   */
+  decolorerCase() {
+    const casesColorees = [...document.querySelectorAll('.chemin')];
+    for( let caseColoree of casesColorees ) { caseColoree.classList.remove('chemin'); }
+  }
 
-    for(let uneDirection of cheminPossible) {
-        const postions = this.getDeplacementsPossible(positionDeDepart, uneDirection, inverser);
-        console.log(postions, uneDirection);
-        for(let postion of postions) {
-            this.colorerCase(postion);
-        }
+  /**
+   *  Déplacer une pièce vers une autre case
+   * @param {String} positionOrigine Position d'originne
+   * @param {String} postionDestination Position de destination
+   */
+  deplacer(positionOrigine, postionDestination) {
+    const destinationNode = this.getCaseNode(postionDestination);
+    const origineNode = this.getCaseNode(positionOrigine);
+
+    if (!destinationNode.classList.contains("chemin") || !origineNode.hasChildNodes()) {
+        console.log("Déplacement pas possible !");
+    } else {
+        destinationNode.appendChild(origineNode.childNodes[0]);
+        console.log(`Déplacement ${positionOrigine} vers ${postionDestination}`);
     }
+    this.deselectionnerCase();
+  }
+  /**
+   * Désélectionner la case sélectionnée
+   */
+  deselectionnerCase() {
+    const caseNode = this.getCaseSelectionneeNode();
+    if(caseNode) {
+        caseNode.classList.remove('selectionner');
+        this.decolorerCase();
+    }
+  }
+  /**
+   * Obtenir le Node de la case ciblé
+   * @param {String} position La position de la case ciblé | Exemple: a1
+   * @returns {Node} Retourne le Node de la case ciblé
+   */
+  getCaseNode(position) { return document.querySelector(`.case[data-position="${position}"]`); }
+  /**
+   * Obtenir la case (Node) actuellement sélectionnée
+   * @returns {Node} Retourne le Node de la case actuellement sélectionnée
+   */
+  getCaseSelectionneeNode() { return document.querySelector('.selectionner'); }
+  /**
+   * Obtenir le couleur de la pièce ciblé
+   * @param {String} position La position de la pièce ciblé | Exemple: a1
+   * @returns {String} Retourne la couleur de la pièce ciblé 
+   */
+  getCouleur(position) {
+    const pièce = this.getPièceNode(position);
+    if (pièce) { return pièce.dataset.couleur; }
+  }
+
+  /**
+   * Obtenir la couleur du joueur courant
+   * @returns {String} Retourne la couleur du joueur courant
+   */
+  getCouleurJoueurCourant() {
+    const div = document.querySelector(".joueur-courant");
+    if (div) { return div.dataset.couleur; }
   }
 
   /**
    * Cette fonction fournit les déplacements possible à partir d'une position de départ.
-   * 
-   * @param {string} positionDeDepart La position de départ | exemple: a5
-   * @param {object} cheminPossible Un vecteur représentant une trajectoire | exemple: {type: 'vecteur', x: -8, y: -8}
-   * @returns Retourne un tableau des positions possible | exemple: ["a1", "b2", "c3"]
+   * @param {string} positionOrigine La position de départ | exemple: a5
+   * @param {object} tabCheminPossible Un vecteur représentant une trajectoire | exemple: {type: 'vecteur', x: -8, y: -8}
+   * @returns {String[]} Retourne un tableau des positions possible | exemple: ["a1", "b2", "c3"]
    */
-  getDeplacementsPossible(positionDeDepart, cheminPossible, inverser = false) {
+  getDeplacementsPossible(positionOrigine, tabCheminPossible, inverser = false) {
     const positions = [];
     const lettres = [];
     const chiffres = [];
-    switch(cheminPossible.type) {
+    switch(tabCheminPossible.type) {
         case "vecteur":
             // On determine les lettres
-            if(cheminPossible.x) {
+            if(tabCheminPossible.x) {
                 // On avance sur l'alphabet. Exemple on passe de A vers B
-                if (cheminPossible.x > 0) {
-                    const lettreLegnth = positionDeDepart[0].charCodeAt() + cheminPossible.x;
-                    for(let i = positionDeDepart[0].charCodeAt()+1; i <= lettreLegnth; i++ ) {
+                if (tabCheminPossible.x > 0) {
+                    const lettreLegnth = positionOrigine[0].charCodeAt() + tabCheminPossible.x;
+                    for(let i = positionOrigine[0].charCodeAt()+1; i <= lettreLegnth; i++ ) {
                         const l = String.fromCharCode(i);
                         lettres.push(l);
                     }
                 // On recule sur l'alphabet. Exemple on passe  de B vers A
-                } else if (cheminPossible.x < 0) {
-                    const lettreLegnth = positionDeDepart[0].charCodeAt() + cheminPossible.x;
-                    for(let i = positionDeDepart[0].charCodeAt()-1; i >= lettreLegnth  ; i-- ) {
+                } else if (tabCheminPossible.x < 0) {
+                    const lettreLegnth = positionOrigine[0].charCodeAt() + tabCheminPossible.x;
+                    for(let i = positionOrigine[0].charCodeAt()-1; i >= lettreLegnth  ; i-- ) {
                         const l = String.fromCharCode(i);
                         lettres.push(l);
                     }
                 }
             } else {
-                const length = Math.abs(cheminPossible.y);
+                const length = Math.abs(tabCheminPossible.y);
                 for(let i = 0; i < length; i++ ) {
-                    lettres.push(positionDeDepart[0]);
+                    lettres.push(positionOrigine[0]);
                 }
             }
     
             // On determine les nombres
-            if(cheminPossible.y) {
-                const nvxChemin = inverser ? -1 * cheminPossible.y : cheminPossible.y;
+            if(tabCheminPossible.y) {
+                const nvxChemin = inverser ? -1 * tabCheminPossible.y : tabCheminPossible.y;
                
                 // On avance sur l'alphabet. Exemple on passe de A vers B
                 if (nvxChemin > 0) {
-                    const chiffreLegnth = positionDeDepart[1]*1 + nvxChemin;
-                    for(let i = positionDeDepart[1]*1 + 1; i <= chiffreLegnth; i++ ) {
+                    const chiffreLegnth = positionOrigine[1]*1 + nvxChemin;
+                    for(let i = positionOrigine[1]*1 + 1; i <= chiffreLegnth; i++ ) {
                         chiffres.push(i);
                     }
                 // On recule sur l'alphabet. Exemple on passe  de B vers A
                 } else if (nvxChemin < 0) {
-                    const chiffreLegnth = positionDeDepart[1]*1 + nvxChemin;
+                    const chiffreLegnth = positionOrigine[1]*1 + nvxChemin;
        
-                    for(let i = positionDeDepart[1]*1-1; i >= chiffreLegnth; i-- ) {
+                    for(let i = positionOrigine[1]*1-1; i >= chiffreLegnth; i-- ) { 
                         chiffres.push(i);
                     }
                 }
             } else {
-                const length = Math.abs(cheminPossible.x);
+                const length = Math.abs(tabCheminPossible.x);
                 for(let i = 0; i < length; i++ ) {
-                    chiffres.push(positionDeDepart[1]);
+                    chiffres.push(positionOrigine[1]);
                 }
             }
             break;
         case "point":
-            lettres.push(String.fromCharCode((positionDeDepart[0].charCodeAt() + cheminPossible.x)));
-            chiffres.push(positionDeDepart[1]*1 + cheminPossible.y);
+            lettres.push(String.fromCharCode((positionOrigine[0].charCodeAt() + tabCheminPossible.x)));
+            chiffres.push(positionOrigine[1]*1 + tabCheminPossible.y);
          break;
     }
-
 
     // On forme les positions et on élimine les postions invalide.
     for(let i=0; i < lettres.length; i++) {
         const chiffre = chiffres[i];
-        const lettre = lettres[i];
+        const lettre  = lettres[i];
 
         // On élimine cette position si elle sort de l'échiquier.
         if (chiffre < 1 || chiffre > 8 || lettre < 'a' || lettre > 'h') { break; }
 
-        if (cheminPossible.regPion) {
-            if (positionDeDepart[1] == 7 && lettre != positionDeDepart[0]) {
-                break;
-            } else if (positionDeDepart[1] == 2 && lettre != positionDeDepart[0]) {
-                break;
+        const pos          = `${lettre}${chiffre}`;
+        const couleurCible = this.getCouleur(pos);
+
+        if (tabCheminPossible.situation) {
+            const couleur = this.getCouleur(positionOrigine);
+            switch(tabCheminPossible.situation) {
+                case 'PRMIER_MOUVEMENT':
+                    if (couleur == "noire" && positionOrigine[1] == 7) { break; } // peut se deplacer de deux 
+                    if (couleur == "blanche" && positionOrigine[1] == 2) { break; } // peut se deplacer de deux 
+                    continue; //  NE peut PAS se deplacer de deux 
+                case 'CAPTURER':
+                    if (couleurCible && couleur != couleurCible) { break; }  // peut capturer
+                    continue; // NE peut PAS capturer
+                case 'NORMAL':
+                    if (!couleurCible) { break; } // peut avancer d'une case à condition qu'il n'y ait pas de pion devant.
+                    continue; // NE peut PAS avancer CAR il y a un pion devant.
             }
         }
-
-        const pos = `${lettre}${chiffre}`;
-        const couleur = this.getCouleur(pos);
         
         // Si cette position à une pièce (donc une couleur) 
-        if (couleur) {
+        if (couleurCible) {
             const couleurCourant = this.getCouleurJoueurCourant();
             // Et que cette couleur est la même que la couleur du joueur courant 
             // Alors on élimine cette couleur.
-            if (couleur == couleurCourant) { break; }
+            if (couleurCible == couleurCourant) { break; }
             
             else { 
                 // Sinon...
                 positions.push(pos);
                 break;
             }
-        } else if(cheminPossible.regPion && cheminPossible.attaque) {
-            break;
-        } 
-
+        } else if(tabCheminPossible.regPion && tabCheminPossible.attaque) { break; } 
         positions.push(pos);
     }
 
     return positions;
   }
-
-  getDeplacementPossible(positionDeDepart, cheminPossible, inverser = false) {
-    let colonneChemin, ligneChemin;
-
-    if (cheminPossible.haut != undefined) {
-        ligneChemin = cheminPossible.haut;
-    } else if (cheminPossible.bas != undefined) {
-        ligneChemin = -1*cheminPossible.bas;
-    }
-
-    if (cheminPossible.droite != undefined) {
-        colonneChemin = cheminPossible.droite;
-    } else if (cheminPossible.gauche != undefined) {
-        colonneChemin = -1*cheminPossible.gauche;
-    }
-
-    if (inverser) { ligneChemin = -1 * ligneChemin; }
-
-    const lettre = String.fromCharCode(positionDeDepart[0].charCodeAt() + colonneChemin);
-    const chiffre = positionDeDepart[1]*1 + ligneChemin;
-
-    if(chiffre < 1 || chiffre > 8 || lettre < 'a' || lettre > 'h') {
-        return "z0";
-    }
-    return `${lettre}${chiffre}`;
+  /**
+   * Obtenir le Node de la pièce ciblé
+   * @param {String} position La position de la pièce ciblé | Exemple: a1
+   * @returns {Node} Retourn le Node de la pièce ciblé
+   */
+  getPièceNode(position) {
+    const caseNode = this.getCaseNode(position);
+    if (caseNode) { return caseNode.childNodes[0]; }
   }
 
-  colorerCase(postion) {
-    if (postion[1] < 1 || postion[1] > 8 || postion[0] < 'a' || postion[0] > 'h') { return; }
-
-    const caseNode = this.getCaseNode(postion);
-    caseNode.classList.add('chemin');
+  /**
+   * Placer une pièce (un Node) sur case (position)
+   * @param {String} position Position receptrice de la pièce | Exemple: a1
+   * @param {Node} pièceNode Élement Node à placer
+   */
+  placerNode(position, pièceNode) {
+    const caseNode = this.getCaseNode(position);
+    caseNode ? caseNode.appendChild(pièceNode) : console.log('err: Placement impossible');
   }
 
-  decolorerCase() {
-    const tab = [...document.querySelectorAll('.chemin')];
-    for( let caseColore of tab ) {
-        caseColore.classList.remove('chemin');
+  /**
+   * Sélectionner une case
+   * @param {String} position La position de la case ciblé | Exemple: a1
+   */
+  selectionnerCase(position) {
+    const caseNode = this.getCaseNode(position);
+    if(caseNode && caseNode.hasChildNodes()) {
+        const pièce = this.getPièceNode(position);
+        const couleurJoueurCourant = document.querySelector('.joueur-courant').dataset.couleur;
+
+        if(couleurJoueurCourant == pièce.dataset.couleur) {
+            this.deselectionnerCase();
+            caseNode.classList.add('selectionner');
+        } else { 
+          console.log("Sélection non autorisée");
+        }
     }
   }
-
 }
